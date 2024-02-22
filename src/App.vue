77920fee3aff139f568f4119822f3894c7f9b527
Anton <a.chertok@akatech.com.ua>
2024-02-22 11:03:28 +0000

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <my-component/>
+
+  </div>
+</template>
+
+<script>
+import MyComponent from './components/MyComponent';
+
+export default {
+  name: 'App',
+  components: {
+    MyComponent
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="root">
+       
+<label>
+Input email
+<input type="email" value="asd"/>
+</label>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MyComponent", 
+    data() {
+        return {
+            // text: "sdasdasdfsf"
+        }
+    }
+}
+</script>
+
+
